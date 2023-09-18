d4241680f42d21667ea481d0fbd7f312b14ff2cf
unknown <eesuolaayot@gmail.com>
2023-09-18 10:53:47 +0100

    comom

--- index.js
+++ index.js
@@ -10,22 +10,20 @@ const upload = multer();
 const fse = require('fs-extra')
 const sharp = require('sharp')
 const path = require('path')
+const TelegramBot = require('node-telegram-bot-api');
 // const bodyParser = require('body-parser')
 
-// db connection to database
-let db;
-
-// creating folder for file uploaded
-fse.ensureDirSync(path.join('public', 'uploaded-photo'))
 
 const {TOKEN, SERVER_URL, chatId} = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${process.env.token}`
 const URI = `/webhook/${TOKEN}`
 const WEBHOOK_URL = SERVER_URL + URI
 
+const bot = new TelegramBot(TOKEN, { polling: true });
+
 const app = express();
 var corsOptions = {
-    origin: ['http://localhost:3000', 'https://luxury-maamoul-41b2e8.netlify.app'],
+    origin: ['http://localhost:3000', 'https://singular-twilight-6a7253.netlify.app'],
     credentials: true,
 };
 app.use(cors(corsOptions));
@@ -41,27 +39,6 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res)=> {
     res.send('workinggg')
 })
-
-app.get('/listing', async(req, res)=> {
-
-    const allData = await db.collection('beerCollection').find().toArray();
-    res.render('lisst', {allData : allData})
-})
-
-app.post('/beer', upload.single('photo'), cleanUp, async(req, res) => {
-    if (req.file) {
-        const photoFileName = `${Date.now()}.jpeg`;
-        await sharp(req.file.buffer).jpeg({quality: 60}).toFile(path.join('public', 'uploaded-photo', photoFileName))
-        req.cleanData.photo = photoFileName;
-    }
-
-    console.log(req.body)
-
-    const info = await db.collection('beerCollection').insertOne(req.cleanData);
-    const newAddedInfo = await db.collection('beerCollection').findOne({_id: new ObjectId(info.insertedId)})
-    res.send(newAddedInfo);
-
-} )
 
 
 
@@ -86,51 +63,21 @@ app.post('/sendMessage', async(req,res)=> {
     })
 
     let text = [];
-    incoming.map(item => text.push(`${item.many} ${item.name} ` ) )
+    // incoming.map(item => text.push(`${item.many} ${item.name} ` ) )
+    incoming.map(item => text.push({qty: item.many, name: item.name}) )
+    const extractValues = text.map(obj => `${obj.name} - ${obj.qty} \n`)
     console.log(text)
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id : process.env.chatId,
-        parse_mode: 'HTML',
-        text: `
-                <b>RN:</b>  ${clean} 📧 
-<b>Order: </b>  ${text.toString()}
-
-              `
-              
-    })
+    bot.sendMessage(chatId, ` <b>RN:</b>  ${clean} 📧 \n ${extractValues}`, {parse_mode: 'HTML'})
     res.send('Order has been sent')
    
 })
 
 
-function cleanUp (req, res, next) {
-    if(typeof req.body.name != "string") req.body.name = "";
-    if(typeof req.body.price != "string") req.body.price = "";
-    if(typeof req.body._id != "string") req.body._id = "";
+const webhookURLs = `https://menu-menu.vercel.app/sendMessage/${TOKEN}`;
+bot.setWebHook(webhookURLs);
 
-    req.cleanData = {
-        name: sanitiseHtml(req.body.name.trim(), { allowedTags: [], allowedAttributes: {}}),
-        price: sanitiseHtml(req.body.price.trim(), { allowedTags: [], allowedAttributes: {} })
-    }
-    next();
-}
-
-const start = async()=> {
-
-    try {
-        
-        const client = new MongoClient ('mongodb+srv://eesuola:timothy@cluster0.7b6p0.mongodb.net/tim?retryWrites=true&w=majority', { useNewUrlParser: true })
-        await client.connect()
-        db = client.db()
-
-        app.listen(process.env.PORT || 80, ()=> {
-            console.log(`app is running on port, ${process.env.PORT || 80}`)
-            // init()
-        })
-
-    } catch (error) {
-        console.log('unable to connect')
-    }
-}
-start();
+app.listen(process.env.PORT || 80, ()=> {
+    console.log(`app is running on port, ${process.env.PORT || 80}`)
+    // init()
+})
 
